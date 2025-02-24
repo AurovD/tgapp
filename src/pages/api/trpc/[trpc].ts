93f28ce2e719d89b39e8a2c82e11a25d261b3774
Aurov Dmitry <dmitry8891@mail.ru>
2025-02-24 16:06:44 +0300

    test user

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -26,6 +26,7 @@ const appRouter = t.router({
 
     hello: t.procedure.input(z.object({ name: z.string() })).query(({ input }) => {
         try {
+            console.log("hello");
             return { message: `Hello, ${input.name}!!!!` };
         } catch (e){
             console.log(e);
