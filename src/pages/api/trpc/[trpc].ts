845e53f857f0a32786d09d43b991795e50c4838a
Aurov Dmitry <dmitry8891@mail.ru>
2025-03-04 15:24:52 +0300

    рефакторинг

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,6 +1,47 @@
 import { createNextApiHandler } from '@trpc/server/adapters/next';
 import { appRouter } from '@/utils/api';
-import { bot } from '@/utils/bot';
+// import { bot } from '@/utils/bot';
+
+
+import { Markup, Telegraf } from 'telegraf';
+import { db } from '@/utils/db';
+import {log} from "node:util";
+
+const BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+
+if (!BOT_TOKEN) {
+    throw new Error('TELEGRAM_BOT_TOKEN is not defined');
+}
+
+
+const bot = new Telegraf(BOT_TOKEN);
+
+bot.command('start', async (ctx) => {
+    const user = ctx.from;
+    const url = "https://tgapp-dohv.onrender.com/";
+
+    console.log('Информация о пользователе:', user);
+
+
+    try {
+        const newUser = await db.collection('users').insertOne({ login: user.username });
+        console.log(newUser);
+    } catch (e) {
+        console.log('Ошибка при добавлении пользователя:', e);
+        throw new Error('Ошибка при добавлении пользователя');
+    }
+
+    const keyboard = Markup.inlineKeyboard([
+        [Markup.button.webApp('Открыть приложение', url)],
+    ]);
+
+    ctx.reply(
+        `Привет, ${user.first_name}! 👋\nНажми на кнопку, чтобы продолжить.`,
+        keyboard
+    );
+});
+
+bot.launch();
 
 export default createNextApiHandler({
     router: appRouter,
--- src/utils/bot.ts
+++ src/utils/bot.ts
@@ -2,14 +2,12 @@ import { Markup, Telegraf } from 'telegraf';
 import { db } from '@/utils/db';
 
 const BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
-console.log("BOT_TOKEN:", BOT_TOKEN);
 
 if (!BOT_TOKEN) {
     throw new Error('TELEGRAM_BOT_TOKEN is not defined');
 }
 
 
-console.log("khlj")
 const bot = new Telegraf(BOT_TOKEN);
 
 bot.command('start', async (ctx) => {
@@ -19,13 +17,13 @@ bot.command('start', async (ctx) => {
     console.log('Информация о пользователе:', user);
 
 
-    // try {
-    //     const newUser = await db.collection('users').insertOne({ login: user.username });
-    //     return { message: 'Пользователь добавлен', userId: newUser.insertedId };
-    // } catch (e) {
-    //     console.log('Ошибка при добавлении пользователя:', e);
-    //     throw new Error('Ошибка при добавлении пользователя');
-    // }
+    try {
+        const newUser = await db.collection('users').insertOne({ login: user.username });
+        return { message: 'Пользователь добавлен', userId: newUser.insertedId };
+    } catch (e) {
+        console.log('Ошибка при добавлении пользователя:', e);
+        throw new Error('Ошибка при добавлении пользователя');
+    }
 
     const keyboard = Markup.inlineKeyboard([
         [Markup.button.webApp('Открыть приложение', url)],
