8dd237883749598135db7a4e0da85930cde6ab37
Aurov Dmitry <dmitry8891@mail.ru>
2025-03-08 14:26:59 +0300

    windows.telegram checking

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,7 +44,7 @@ export default function HomePage() {
                 });
             }
         }
-    }, [sendUserData]);
+    }, []);
 
     if (isLoading) return <p>Loading...</p>;
     return (<>
