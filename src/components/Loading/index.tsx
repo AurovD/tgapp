06783c70164c3b0a8390251693d1b070b9f5ec61
Aurov Dmitry <dmitry8891@mail.ru>
2025-03-15 13:22:40 +0300

    loading page

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,7 +1,16 @@
+// import sample from '../../../public/video.mp4';
+
 export default function Loading() {
     return (
-        <div className={"card"}>
-            <p>Загрузка данных пользователя...ля ля ля</p>
-        </div>
+        <>
+            <div className={"loading"}>
+                <video className='videoTag' autoPlay loop muted>
+                    <source src={"video.mp4"} type='video/mp4'/>
+                </video>
+                <div className={"text"}>
+                        <p>Привет!</p>
+                </div>
+            </div>
+        </>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,49 +19,49 @@ declare global {
     }
 }
 
-export default function HomePage() {
-
-    return (
-        <>
-            <Loading/>
-    </>
-    );
-}
 // export default function HomePage() {
-//     const [user, setUser] = useState<User | null>(null);
-//     const sendUserData = trpc.user.useMutation({
-//         onError: (error) => {
-//             console.error("Ошибка при отправке данных:", error);
-//             alert("Произошла ошибка. Попробуйте позже.");
-//             window.Telegram?.WebApp?.close();
-//         },
-//     });
-//
-//     useEffect(() => {
-//         if (window.Telegram?.WebApp) {
-//             const userData = window.Telegram.WebApp.initDataUnsafe?.user;
-//             if (userData) {
-//                 setUser(userData);
-//
-//                 sendUserData.mutate({
-//                     id: userData.id,
-//                     first_name: userData.first_name,
-//                     username: userData.username || "Без логина",
-//                 });
-//             } else {
-//                 alert("Не удалось получить данные пользователя.");
-//                 window.Telegram?.WebApp?.close();
-//             }
-//         }
-//     }, []);
 //
 //     return (
 //         <>
-//         {user ? (
-//             <p>Привет, {user.first_name}!</p>
-//         ) : (
-//             <Loading></Loading>
-//         )}
+//             <Loading/>
 //     </>
 //     );
 // }
+export default function HomePage() {
+    const [user, setUser] = useState<User | null>(null);
+    const sendUserData = trpc.user.useMutation({
+        onError: (error) => {
+            console.error("Ошибка при отправке данных:", error);
+            alert("Произошла ошибка. Попробуйте позже.");
+            window.Telegram?.WebApp?.close();
+        },
+    });
+
+    useEffect(() => {
+        if (window.Telegram?.WebApp) {
+            const userData = window.Telegram.WebApp.initDataUnsafe?.user;
+            if (userData) {
+                setUser(userData);
+
+                sendUserData.mutate({
+                    id: userData.id,
+                    first_name: userData.first_name,
+                    username: userData.username || "Без логина",
+                });
+            } else {
+                // alert("Не удалось получить данные пользователя.");
+                window.Telegram?.WebApp?.close();
+            }
+        }
+    }, []);
+
+    return (
+        <>
+        {user ? (
+            <p>Привет, {user.first_name}!</p>
+        ) : (
+            <Loading></Loading>
+        )}
+    </>
+    );
+}
