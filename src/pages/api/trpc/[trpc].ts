27732f6017c40366464718b2cefbc0ae8da4b1d1
Aurov Dmitry <dmitry8891@mail.ru>
2026-02-06 16:22:04 +0300

    mongodb connection

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -2,15 +2,24 @@ import { createNextApiHandler } from '@trpc/server/adapters/next';
 import { router } from '@/utils/api';
 import { bot } from '@/utils/bot';
 import { connectDb } from '@/utils/db';
+
 // Подключаемся к базе данных
-connectDb();
-bot.launch()
-    .then(() => console.log('Telegram bot started!'))
-    .catch((err) => console.error('Error launching bot:', err));
+void connectDb().catch((err) => console.error('MongoDB connection error:', err));
+
+declare global {
+    // eslint-disable-next-line no-var
+    var __botStarted: boolean | undefined;
+}
+
+if (!global.__botStarted) {
+    global.__botStarted = true;
+    bot.launch()
+        .then(() => console.log('Telegram bot started!'))
+        .catch((err) => console.error('Error launching bot:', err));
+}
 
 export type AppRouter = typeof router;
 
 export default createNextApiHandler({
     router,
 });
-
--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,27 +1,48 @@
-import  { MongoClient, ServerApiVersion } from 'mongodb';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
 const { DB_USER_NAME, DB_PASSWORD } = process.env;
-const uri = `mongodb+srv://${DB_USER_NAME}:${DB_PASSWORD}@cluster0.fhosm.mongodb.net/?appName=Cluster0`;
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
+
+if (!DB_USER_NAME || !DB_PASSWORD) {
+    throw new Error('MongoDB credentials are missing');
+}
+
+const uri = `mongodb+srv://${DB_USER_NAME}:${DB_PASSWORD}@cluster0.fhosm.mongodb.net/tgapp?retryWrites=true&w=majority&appName=Cluster0`;
+
+const options = {
     serverApi: {
         version: ServerApiVersion.v1,
         strict: true,
         deprecationErrors: true,
-    }
-});
-async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-    }
+    },
+};
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+declare global {
+    // eslint-disable-next-line no-var
+    var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
-run().catch(console.dir);
+
+if (process.env.NODE_ENV === 'development') {
+    if (!global._mongoClientPromise) {
+        client = new MongoClient(uri, options);
+        global._mongoClientPromise = client.connect();
+    }
+    clientPromise = global._mongoClientPromise;
+} else {
+    client = new MongoClient(uri, options);
+    clientPromise = client.connect();
+}
+
+export const connectDb = async () => {
+    await clientPromise;
+    return client;
+};
+
+export { clientPromise };
+
+
 
 // import mongoose from 'mongoose';
 //
@@ -44,4 +65,3 @@ run().catch(console.dir);
 // };
 //
 // export { connectDb, mongoose };
-
